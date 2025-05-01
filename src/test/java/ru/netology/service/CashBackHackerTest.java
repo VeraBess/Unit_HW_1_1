1492461a88e3973414f5862f0677c067312fab5a
vera bessonova <v2112365@gmail.com>
2025-05-01 18:04:50 +0300

    Task 1 Part 2 TestNG add change tests

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -1,44 +1,50 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-class CashBackHackerTest {
+import static org.testng.Assert.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
+public class CashBackHackerTest {
 
     @Test
-    void buyMore100Rub() {   //докупить на 100 руб
+    public void buyMore100Rub() {   //докупить на 100 руб
         CashBackHacker cashBackHacker = new CashBackHacker();
 
         int actual = cashBackHacker.remain(900);
         int expected = 100;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
     @Test
-    void buyMore0Rub() { //докупить на 0 руб если куплено на 1000
+    public void buyMore0Rub() { //докупить на 0 руб если куплено на 1000
         CashBackHacker cashBackHacker = new CashBackHacker();
 
         int actual = cashBackHacker.remain(1_000);
         int expected = 1_000;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
     @Test
-    void buyMore999Rub() { //докупить на 999 руб если куплено на 1001
+    public void buyMore999Rub() { //докупить на 999 руб если куплено на 1001
         CashBackHacker cashBackHacker = new CashBackHacker();
 
         int actual = cashBackHacker.remain(1001);
         int expected = 999;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
+
     @Test
-    void buyMore1000Rub() { //докупить на 1000 руб если куплено на 0
+    public void buyMore1000Rub() { //докупить на 1000 руб если куплено на 0
         CashBackHacker cashBackHacker = new CashBackHacker();
 
         int actual = cashBackHacker.remain(0);
         int expected = 1000;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
