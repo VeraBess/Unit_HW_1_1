f4cbb070ea6df914463a720e1655a19a3b2301df
vera bessonova <v2112365@gmail.com>
2025-05-01 16:20:17 +0300

    Task 1 JUnit5 add class & tests

---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CashBackHackerTest {
+
+    @Test
+    void buyMore100Rub() {   //докупить на 100 руб
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void buyMore0Rub() { //докупить на 0 руб если куплено на 1000
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(1_000);
+        int expected = 1_000;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void buyMore999Rub() { //докупить на 999 руб если куплено на 1001
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(1001);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void buyMore1000Rub() { //докупить на 1000 руб если куплено на 0
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(0);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+}
