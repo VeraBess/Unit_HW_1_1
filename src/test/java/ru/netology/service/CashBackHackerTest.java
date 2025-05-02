585b73d728ce3967a0b8eebab1c314ab3b446646
vera bessonova <v2112365@gmail.com>
2025-05-02 13:18:01 +0300

    Task 1 JUnit5 add change test for foll CI

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -19,7 +19,7 @@ class CashBackHackerTest {
         CashBackHacker cashBackHacker = new CashBackHacker();
 
         int actual = cashBackHacker.remain(1_000);
-        int expected = 1_000;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
